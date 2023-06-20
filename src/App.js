1c1f05271643d704c45f7027682854abee9c0cb4
Mehedi Hasan <mhjibon.info@gmail.com>
2023-06-20 23:16:25 +0600

    check out event listener, and useState inside components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <District name="Noakhali" special="bivag"></District>
+      <District name="Bramonbaria" special="joda akbar"></District>
+      <District name="Sumilla" special="moyna and moti"></District>
     </div>
   );
+}
+
+const districtStyle = {
+  backgroundColor: 'lightblue',
+  padding: '20px',
+  borderRadius: '20px',
+  margin:'20px'
+}
+
+function District(props){
+  const [power, setPower] =  useState(1);
+
+  const boostPower = () =>{
+    const newPower = power * 2;
+    setPower(newPower);
+  }
+
+  return(
+    <div style={districtStyle}>
+      <h2> Name: {props.name}</h2>
+      <p> Specialty: {props.special}</p>
+      <h4> Power: {power}</h4>
+      <button onClick={boostPower}> Boost The Power </button>
+    </div>
+  )
 }
 
 export default App;
