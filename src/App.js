2db0f7bf24d3045cd2abb8ec68119727c74acfec
Mehedi Hasan <mhjibon.info@gmail.com>
2023-06-20 23:27:08 +0600

    load data using the useEffect and display data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
   return (
     <div className="App">
+      <LoadPost></LoadPost>
       <District name="Noakhali" special="bivag"></District>
       <District name="Bramonbaria" special="joda akbar"></District>
       <District name="Sumilla" special="moyna and moti"></District>
     </div>
   );
+}
+
+function LoadPost(){
+  const [posts, setPosts] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => setPosts(data))
+  },[])
+  return(
+    <div>
+      <h1> Posts: {posts.length}</h1>
+      {
+        posts.map(post => <Post 
+          post={post}
+          key={post.id}
+        ></Post>)
+      }
+    </div>
+  )
+}
+
+function Post(props){
+  const {title, body} = props.post
+  return(
+    <div style={{backgroundColor: 'lightgray' , padding: '20px', margin: '20px', border: '4px solid salmon' , borderRadius: '10px'}}>
+      <h2> Title: {title}</h2>
+      <p> Body: {body}</p>
+    </div>
+  )
 }
 
 const districtStyle = {
