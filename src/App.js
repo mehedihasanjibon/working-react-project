ac8cfa9b790ed16bde5ab10661c564a17cf2b394
Mehedi Hasan <mhjibon.info@gmail.com>
2023-08-29 23:43:52 +0600

    data json file create

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import SideCart from './components/SideCart/SideCart';
 
 const App = () => {
   return (
@@ -10,12 +11,12 @@ const App = () => {
         <Header></Header>
       </div>
 
-      <div>
-        <div className='home-container'>
+      <div className='main row'>
+        <div className='home-container col-md-8'>
           <Home></Home>
         </div>
-        <div className='sideCart'>
-
+        <div className='sideCart col-md-4 card'>
+          <SideCart></SideCart>
         </div>
       </div>
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect( () => {
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }, []);
+
     return (
         <div>
             <h3> this is home content </h3>
