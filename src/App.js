fb8b69551d84e02664a75319c71f982e4d03626a
Mehedi Hasan <mhjibon.info@gmail.com>
2023-09-13 21:28:02 +0600

    header section done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
-import SideCart from './components/SideCart/SideCart';
-import Home from './components/Home/Home';
 
 const App = () => {
   return (
     <div className='container'>
-      <div className='header m-auto mb-3'>
+      <div className='header'>
         <Header></Header>
       </div>
-
-      <div className='main row'>
-        <div className='home-container col-md-8'>
-          <Home></Home>
-        </div>
-        <div className='sideCart col-md-4 card'>
-          <SideCart></SideCart>
-        </div>
-      </div>
-
     </div>
   );
 };
